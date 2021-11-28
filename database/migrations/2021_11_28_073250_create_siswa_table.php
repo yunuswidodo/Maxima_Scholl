230d5cd9de55e96006a67ffc4903e8160979cd73
yunuswidodo <younews372@gmail.com>
2021-11-28 18:20:29 +0700

    firt commit

---
+++ database/migrations/2021_11_28_073250_create_siswa_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('NIS', 20)->nullable();
+            $table->string('Nama', 50)->nullable();
+            $table->string('Alamat', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
